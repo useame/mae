803632001d33863de39dffcce09e56ea43b91432
jackpurcell <ljczmh@gmail.com>
2022-03-08 22:31:44 +0800

    FIX HUMAN36M dataloader
    [21:10:13.161092] Epoch: [801]  [12820/78047]  eta: 2:56:09  lr: 0.000000  loss: 119.8927 (117.6745)  time: 0.1615  data: 0.0001  max mem: 3627
    Traceback (most recent call last):
      File "/home/ljc/Github/mae/main_pretrain_before.py", line 253, in <module>
        main(args)
      File "/home/ljc/Github/mae/main_pretrain_before.py", line 223, in main
        train_stats = train_one_epoch(
      File "/home/ljc/Github/mae/engine_pretrain.py", line 49, in train_one_epoch
        loss = model(samples)
      File "/home/ljc/anaconda3/lib/python3.8/site-packages/torch/nn/modules/module.py", line 1102, in _call_impl
        return forward_call(*input, **kwargs)
      File "/home/ljc/Github/mae/models_mae_before.py", line 289, in forward
        dy = self.forward_encoder(imgs)
      File "/home/ljc/Github/mae/models_mae_before.py", line 229, in forward_encoder
        x = blk(x)
      File "/home/ljc/anaconda3/lib/python3.8/site-packages/torch/nn/modules/module.py", line 1102, in _call_impl
        return forward_call(*input, **kwargs)
      File "/home/ljc/anaconda3/lib/python3.8/site-packages/timm/models/vision_transformer.py", line 141, in forward
        x = x + self.drop_path(self.attn(self.norm1(x)))
      File "/home/ljc/anaconda3/lib/python3.8/site-packages/torch/nn/modules/module.py", line 1102, in _call_impl
        return forward_call(*input, **kwargs)
      File "/home/ljc/anaconda3/lib/python3.8/site-packages/timm/models/vision_transformer.py", line 120, in forward
        x = (attn @ v).transpose(1, 2).reshape(B, N, C)
    RuntimeError: CUDA error: device-side assert triggered
    CUDA kernel errors might be asynchronously reported at some other API call,so the stacktrace below might be incorrect.
    For debugging consider passing CUDA_LAUNCH_BLOCKING=1.

--- models_mae_before.py
+++ models_mae_before.py
@@ -207,6 +207,13 @@ class MaskedAutoencoderViT(nn.Module):
         # unshuffle to get the binary mask
         mask = torch.gather(mask, dim=1, index=ids_restore)
 
+
+
+
+
+
+
+
         return x_masked, mask, ids_restore
     # here
     def forward_encoder(self, x):
@@ -223,7 +230,8 @@ class MaskedAutoencoderViT(nn.Module):
         cls_token = self.cls_token + self.pos_embed[:, :1, :]
         cls_tokens = cls_token.expand(x.shape[0], -1, -1)
         x = torch.cat((cls_tokens, x), dim=1)
-
+        if x.shape != torch.Size([4, 257, 768]):
+            print("EMMMMMMMMMMMMMMMMMMMMMMMMM")
         # apply Transformer blocks
         for blk in self.blocks:
             x = blk(x)
